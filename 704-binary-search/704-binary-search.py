40f63b5401706429a6174c0afdf6f801d5376ebb
Satyam Tripathi <satyamtripathi5554@gmail.com>
2022-02-25 11:34:20 +0530

    Time: 391 ms (30.13%), Space: 15.4 MB (85.28%) - LeetHub

---
+++ 704-binary-search/704-binary-search.py
@@ -0,0 +1,14 @@
+class Solution:
+    def search(self, nums: List[int], target: int) -> int:
+        def solve(nums, st, end, tar):
+            if end>=st:
+                mid=(st+end)>>1
+                if nums[mid]==tar:
+                    return mid
+                elif nums[mid]<tar:
+                    return solve(nums, mid+1, end, tar)
+                else:
+                    return solve(nums, st, mid-1, tar)
+            return -1
+    
+        return solve(nums, 0, len(nums)-1, target)
