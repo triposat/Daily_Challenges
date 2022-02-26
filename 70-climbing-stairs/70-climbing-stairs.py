379c5f41871818cee7f6da2133674c3243e1b864
Satyam Tripathi <satyamtripathi5554@gmail.com>
2022-02-26 19:10:44 +0530

    Time: 38 ms (60.41%), Space: 13.8 MB (82.85%) - LeetHub

--- 70-climbing-stairs/70-climbing-stairs.py
+++ 70-climbing-stairs/70-climbing-stairs.py
@@ -1,12 +1,23 @@
+# Memoization:
+# class Solution:
+#     def climbStairs(self, n: int) -> int:
+#         dp = [0]*(n+1)
+
+#         def solve(n):
+#             if n < 4:
+#                 return n
+#             if dp[n] != 0:
+#                 return dp[n]
+#             dp[n] = solve(n-1)+solve(n-2)
+#             return dp[n]
+#         return solve(n)
+# Tabulation:
 class Solution:
     def climbStairs(self, n: int) -> int:
         dp = [0]*(n+1)
-
-        def solve(n):
-            if n < 4:
-                return n
-            if dp[n] != 0:
-                return dp[n]
-            dp[n] = solve(n-1)+solve(n-2)
-            return dp[n]
-        return solve(n)
+        dp[0]=1
+        dp[1]=1
+        for i in range(2, n+1):
+            dp[i] = dp[i-1]+dp[i-2]
+            print(dp[i])
+        return dp[n]
