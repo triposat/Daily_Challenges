6a786698ddd50e9053092e8099b2ea1e677f85d5
Satyam Tripathi <satyamtripathi5554@gmail.com>
2022-02-26 16:06:13 +0530

    Time: 232 ms (81.47%), Space: 25.4 MB (53.92%) - LeetHub

--- 189-rotate-array/189-rotate-array.py
+++ 189-rotate-array/189-rotate-array.py
@@ -3,5 +3,19 @@ class Solution:
         """
         Do not return anything, modify nums in-place instead.
         """
-        for _ in range(k):
-            nums.insert(0, nums.pop())
+        # Approach 1:
+        # for _ in range(k):
+        #     nums.insert(0, nums.pop())
+            
+        # Approach 2:
+        def rev(i, j):
+            while i<j:
+                nums[i], nums[j] = nums[j], nums[i]
+                i+=1
+                j-=1
+        n=len(nums)
+        k=k%n
+        rev(0, n-1)
+        rev(0, k-1)
+        rev(k, n-1)
+        
