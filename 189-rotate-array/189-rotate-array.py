2baf55fe9feba5c8ae53cc225d19d1ff462512c1
Satyam Tripathi <satyamtripathi5554@gmail.com>
2022-02-26 16:06:56 +0530

    Time: 351 ms (46.04%), Space: 25.4 MB (87.10%) - LeetHub

--- 189-rotate-array/189-rotate-array.py
+++ 189-rotate-array/189-rotate-array.py
@@ -6,16 +6,15 @@ class Solution:
         # Approach 1:
         # for _ in range(k):
         #     nums.insert(0, nums.pop())
-            
+
         # Approach 2:
         def rev(i, j):
-            while i<j:
+            while i < j:
                 nums[i], nums[j] = nums[j], nums[i]
-                i+=1
-                j-=1
-        n=len(nums)
-        k=k%n
+                i += 1
+                j -= 1
+        n = len(nums)
+        k = k % n
         rev(0, n-1)
         rev(0, k-1)
         rev(k, n-1)
-        
