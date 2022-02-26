efc056b8049398fd57d8f1ec122ab1fcff0220ff
Satyam Tripathi <satyamtripathi5554@gmail.com>
2022-02-26 13:45:55 +0530

    Time: 56 ms (34.23%), Space: 14.1 MB (64.29%) - LeetHub

--- 101-symmetric-tree/101-symmetric-tree.py
+++ 101-symmetric-tree/101-symmetric-tree.py
@@ -5,20 +5,19 @@
 #         self.left = left
 #         self.right = right
 class Solution:
-      def isSymmetric(self, root):
-            if root is None:
-                return True
-            stack = [(root.left, root.right)]
-            while stack:
-                print(stack[-1])
-                left, right = stack.pop()
-                if left is None and right is None:
-                    continue
-                if left is None or right is None:
-                    return False
-                if left.val == right.val:
-                    stack.append((left.left, right.right))
-                    stack.append((left.right, right.left))
-                else:
-                    return False
+    def isSymmetric(self, root: Optional[TreeNode]) -> bool:
+        if not root:
             return True
+        stack = [(root.left, root.right)]
+        while stack:
+            left, right = stack.pop()
+            if left is None and right is None:
+                continue
+            if left is None or right is None:
+                return False
+            if left.val == right.val:
+                stack.append((left.left, right.right))
+                stack.append((left.right, right.left))
+            else:
+                return False
+        return True
