dbb83afac1268d0fc7d6a7e6277d334e807ab643
Satyam Tripathi <satyamtripathi5554@gmail.com>
2022-02-27 23:13:49 +0530

    Time: 160 ms (97.70%), Space: 15.6 MB (42.97%) - LeetHub

--- 283-move-zeroes/283-move-zeroes.py
+++ 283-move-zeroes/283-move-zeroes.py
@@ -3,11 +3,11 @@ class Solution:
         """
         Do not return anything, modify nums in-place instead.
         """
-        j=0
+        j = 0
         for i in range(len(nums)):
-            if nums[i]!=0:
+            if nums[i] != 0:
                 nums[j] = nums[i]
-                j+=1
+                j += 1
         for i in range(j, len(nums)):
-            nums[i]=0
+            nums[i] = 0
         # return nums
