c0304c1ae7bf7191e85dcec28464677ed9b48867
Satyam Tripathi <satyamtripathi5554@gmail.com>
2022-03-01 09:39:08 +0530

    Time: 180 ms (28.36%), Space: 21.5 MB (20.83%) - LeetHub

--- 338-counting-bits/338-counting-bits.py
+++ 338-counting-bits/338-counting-bits.py
@@ -1,16 +1,21 @@
 class Solution:
     def countBits(self, n: int) -> List[int]:
-        @lru_cache(None)
+        memo = [0]*(n+1)
+
         def solve(n):
-            if n==0:
+            if n == 0:
                 return 0
-            if n==1:
+            if n == 1:
                 return 1
-            if n%2==0:
-                return solve(n//2)
+            if memo[n]:
+                return memo[n]
+            if n % 2 == 0:
+                memo[n] = solve(n//2)
+                return memo[n]
             else:
-                return 1+solve(n//2)
-        res=[0]*(n+1)
+                memo[n] = 1+solve(n//2)
+                return memo[n]
+        res = [0]*(n+1)
         for i in range(0, n+1):
             res[i] = solve(i)
         return res
