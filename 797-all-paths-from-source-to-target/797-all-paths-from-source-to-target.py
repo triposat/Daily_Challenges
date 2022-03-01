af7487c2bc3155549412a3f9a59512b7fe050256
Satyam Tripathi <satyamtripathi5554@gmail.com>
2022-03-01 19:04:12 +0530

    Time: 143 ms (46.97%), Space: 15.7 MB (37.62%) - LeetHub

--- 797-all-paths-from-source-to-target/797-all-paths-from-source-to-target.py
+++ 797-all-paths-from-source-to-target/797-all-paths-from-source-to-target.py
@@ -1,18 +1,33 @@
 class Solution:
     def allPathsSourceTarget(self, graph: List[List[int]]) -> List[List[int]]:
         # Iterative Approach:
+        # if not graph:
+        #     return []
+        # memo = {}
+        # for idx, val in enumerate(graph):
+        #     memo[idx] = val
+        # stack = [(0, [0])]
+        # res = []
+        # while stack:
+        #     node, path = stack.pop()
+        #     if node == len(graph)-1:
+        #         res.append(path)
+        #     for i in memo[node]:
+        #         stack.append((i, path+[i]))
+        # return res
+    
+        # Recursive Approach
         if not graph:
             return []
-        hey={}
-        for idx, val in enumerate(graph):
-            hey[idx]=val
-        stack=[(0, [0])]
-        res=[]
-        while stack:
-            node, path = stack.pop()
-            if node==len(graph)-1:
+        res = []
+
+
+        def solve(node, path):
+            if node == len(graph)-1:
                 res.append(path)
-            for me in hey[node]:
-                stack.append((me, path+[me]))
+            for i in graph[node]:
+                solve(i, path+[i])
+
+
+        solve(0, [0])
         return res
-        
