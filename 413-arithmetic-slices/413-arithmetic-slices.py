e7c43793c2ec2046e4dad685695b6ed41ae5551b
Satyam Tripathi <satyamtripathi5554@gmail.com>
2022-03-03 10:16:36 +0530

    Time: 51 ms (58.95%), Space: 14.1 MB (75.02%) - LeetHub

--- 413-arithmetic-slices/413-arithmetic-slices.py
+++ 413-arithmetic-slices/413-arithmetic-slices.py
@@ -1,10 +1,25 @@
+# O(n) Space
+# class Solution:
+#     def numberOfArithmeticSlices(self, nums: List[int]) -> int:
+#         n = len(nums)
+#         if n < 3:
+#             return 0
+#         memo = [0]*(n)
+#         for i in range(2, n):
+#             if nums[i]-nums[i-1] == nums[i-1]-nums[i-2]:
+#                 memo[i] = memo[i-1]+1
+#         return sum(memo)
+# O(1) Space
 class Solution:
     def numberOfArithmeticSlices(self, nums: List[int]) -> int:
-        n=len(nums)
-        if n<3:
+        n = len(nums)
+        if n < 3:
             return 0
-        memo=[0]*(n)
+        res = cur = 0
         for i in range(2, n):
             if nums[i]-nums[i-1] == nums[i-1]-nums[i-2]:
-                memo[i] = memo[i-1]+1
-        return sum(memo)
+                cur += 1
+                res += cur
+            else:
+                cur = 0
+        return res
