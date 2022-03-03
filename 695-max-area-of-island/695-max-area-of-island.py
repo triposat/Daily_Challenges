95bb458c91ea279549640763b60a8a7c2de0096c
Satyam Tripathi <satyamtripathi5554@gmail.com>
2022-03-03 16:15:35 +0530

    Time: 140 ms (90.23%), Space: 16.5 MB (58.48%) - LeetHub

--- 695-max-area-of-island/695-max-area-of-island.py
+++ 695-max-area-of-island/695-max-area-of-island.py
@@ -1,13 +1,34 @@
+# class Solution:
+#     def markAll(self, row, col, i, j, grid):
+#         if i < 0 or i >= row or j < 0 or j >= col or grid[i][j] != 1:
+#             return 0
+#         grid[i][j] = 2
+#         a = self.markAll(row, col, i, j-1, grid) + 1
+#         b = self.markAll(row, col, i, j+1, grid) + 1
+#         c = self.markAll(row, col, i-1, j, grid)+1
+#         d = self.markAll(row, col, i+1, j, grid)+1
+#         return (a+b+c+d)
+
+#     def maxAreaOfIsland(self, grid: List[List[int]]) -> int:
+#         row = len(grid)
+#         col = len(grid[0])
+#         num = 0
+#         for i in range(row):
+#             for j in range(col):
+#                 if grid[i][j] == 1:
+#                     num = max(num, self.markAll(row, col, i, j, grid))
+#         return num//4
+    
 class Solution:
     def markAll(self, row, col, i, j, grid):
         if i < 0 or i >= row or j < 0 or j >= col or grid[i][j] != 1:
             return 0
         grid[i][j] = 2
-        a = self.markAll(row, col, i, j-1, grid) + 1
-        b = self.markAll(row, col, i, j+1, grid) + 1
-        c = self.markAll(row, col, i-1, j, grid)+1
-        d = self.markAll(row, col, i+1, j, grid)+1
-        return (a+b+c+d)
+        self.cnt += 1
+        self.markAll(row, col, i, j-1, grid)
+        self.markAll(row, col, i, j+1, grid)
+        self.markAll(row, col, i-1, j, grid)
+        self.markAll(row, col, i+1, j, grid)
 
     def maxAreaOfIsland(self, grid: List[List[int]]) -> int:
         row = len(grid)
@@ -15,6 +36,10 @@ class Solution:
         res = 0
         for i in range(row):
             for j in range(col):
+                self.cnt = 0
                 if grid[i][j] == 1:
-                    res = max(res, self.markAll(row, col, i, j, grid))
-        return res//4
+                    self.markAll(row, col, i, j, grid)
+                    res = max(res, self.cnt)
+        return res
+
+    
