eede2efd1e692cf80a80634674f3d8b5e58ed13d
Satyam Tripathi <satyamtripathi5554@gmail.com>
2022-03-03 23:45:11 +0530

    Added solution - LeetHub

--- 0 - 1 Knapsack Problem - GFG/0-1-knapsack-problem.py
+++ 0 - 1 Knapsack Problem - GFG/0-1-knapsack-problem.py
@@ -1,3 +1,24 @@
+# Memoization
+class Solution:
+    # Function to return max value that can be put in knapsack of capacity W.
+    def knapSack(self, W, wt, val, n):
+        dp = [[-1]*(W+1) for _ in range(n+1)]
+
+        def solve(W, wt, val, n):
+            if W == 0 or n == 0:
+                dp[n][W] = 0
+                return dp[n][W]
+            if dp[n][W] != -1:
+                return dp[n][W]
+            if wt[n-1] <= W:
+                dp[n][W] = max(val[n-1]+solve(W-wt[n-1], wt,
+                                              val, n-1), solve(W, wt, val, n-1))
+            elif wt[n-1] > W:
+                dp[n][W] = solve(W, wt, val, n-1)
+            return dp[n][W]
+
+        return solve(W, wt, val, n)
+
 # Top-down Approach
 class Solution:
     # Function to return max value that can be put in knapsack of capacity W.
@@ -12,7 +33,7 @@ class Solution:
                 else:
                     dp[i][j] = dp[i-1][j]
         return dp[n][W]
-        
+
         
 #{ 
 #  Driver Code Starts
