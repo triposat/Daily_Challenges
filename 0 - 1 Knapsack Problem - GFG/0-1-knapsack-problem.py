8d1981cecb5a7cf739b4b9e031ba1136ed051058
Satyam Tripathi <satyamtripathi5554@gmail.com>
2022-03-03 23:43:34 +0530

    Added solution - LeetHub

--- 0 - 1 Knapsack Problem - GFG/0-1-knapsack-problem.py
+++ 0 - 1 Knapsack Problem - GFG/0-1-knapsack-problem.py
@@ -1,21 +1,19 @@
+# Top-down Approach
 class Solution:
     # Function to return max value that can be put in knapsack of capacity W.
     def knapSack(self, W, wt, val, n):
-        def solve(W, wt, val, n, dp):
-            if W == 0 or n == 0:
-                dp[n][W] = 0
-                return dp[n][W]
-            if dp[n][W] != -1:
-                return dp[n][W]
-            if wt[n-1] <= W:
-                dp[n][W] = max(val[n-1]+solve(W-wt[n-1], wt,
-                                              val, n-1, dp), solve(W, wt, val, n-1, dp))
-            elif wt[n-1] > W:
-                dp[n][W] = solve(W, wt, val, n-1, dp)
-            return dp[n][W]
-        dp = [[-1]*(W+1) for _ in range(n+1)]
-        return solve(W, wt, val, n, dp)
-
+        dp = [[0]*(W+1) for _ in range(n+1)]
+        for i in range(n+1):
+            for j in range(W+1):
+                if i == 0 or j == 0:
+                    dp[i][j] = 0
+                elif wt[i-1] <= j:
+                    dp[i][j] = max(val[i-1]+dp[i-1][j-wt[i-1]], dp[i-1][j])
+                else:
+                    dp[i][j] = dp[i-1][j]
+        return dp[n][W]
+        
+        
 #{ 
 #  Driver Code Starts
 #Initial Template for Python 3
