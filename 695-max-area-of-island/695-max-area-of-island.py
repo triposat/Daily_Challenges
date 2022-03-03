3141080363a9b2c23cbd8a2301bb44b3f86d48a3
Satyam Tripathi <satyamtripathi5554@gmail.com>
2022-03-03 16:04:53 +0530

    Time: 214 ms (42.85%), Space: 17.6 MB (30.05%) - LeetHub

--- 695-max-area-of-island/695-max-area-of-island.py
+++ 695-max-area-of-island/695-max-area-of-island.py
@@ -3,8 +3,8 @@ class Solution:
         if i < 0 or i >= row or j < 0 or j >= col or grid[i][j] != 1:
             return 0
         grid[i][j] = 2
-        a = self.markAll(row, col, i, j-1, grid) +1
-        b = self.markAll(row, col, i, j+1, grid) +1
+        a = self.markAll(row, col, i, j-1, grid) + 1
+        b = self.markAll(row, col, i, j+1, grid) + 1
         c = self.markAll(row, col, i-1, j, grid)+1
         d = self.markAll(row, col, i+1, j, grid)+1
         return (a+b+c+d)
@@ -12,11 +12,9 @@ class Solution:
     def maxAreaOfIsland(self, grid: List[List[int]]) -> int:
         row = len(grid)
         col = len(grid[0])
-        num = 0
-        cnt=0
+        res = 0
         for i in range(row):
             for j in range(col):
                 if grid[i][j] == 1:
-                    num = max(num, self.markAll(row, col, i, j, grid))
-        return num//4
-    
+                    res = max(res, self.markAll(row, col, i, j, grid))
+        return res//4
