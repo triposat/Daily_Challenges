e524a9d9eb33c46b8dea19e6372c0735c1571573
Satyam Tripathi <satyamtripathi5554@gmail.com>
2022-03-04 12:29:10 +0530

    Time: 80 ms (98.54%), Space: 14 MB (81.55%) - LeetHub

--- 799-champagne-tower/799-champagne-tower.py
+++ 799-champagne-tower/799-champagne-tower.py
@@ -1,22 +1,26 @@
 class Solution:
     def champagneTower(self, poured: int, query_row: int, query_glass: int) -> float:
-        # res=[[0]*i for i in range(1, 102)]
-        # res[0][0]=poured
-        # for i in range(query_row+1):
-        #     for j in range(i+1):
-        #         Q=(res[i][j]-1)/2
-        #         if Q>0:
-        #             res[i+1][j]+=Q
-        #             res[i+1][j+1]+=Q
-        # return min(1, res[query_row][query_glass])
-        hey=[poured]
-        
+        # Extra Space
+        res = [[0]*i for i in range(1, 102)]
+        res[0][0] = poured
+        for i in range(query_row+1):
+            for j in range(i+1):
+                nextPour = (res[i][j]-1)/2
+                if nextPour > 0:
+                    res[i+1][j] += nextPour
+                    res[i+1][j+1] += nextPour
+        return min(1, res[query_row][query_glass])
+
+class Solution:
+    def champagneTower(self, poured: int, query_row: int, query_glass: int) -> float:
+        # Optimal Space
+        glass = [poured]
         for _ in range(query_row):
-            me=[0]*(len(hey)+1)
-            for j in range(len(hey)):
-                Q=(hey[j]-1)/2
-                if Q>0:
-                    me[j]+=Q
-                    me[j+1]+=Q    
-            hey=me
-        return min(1, hey[query_glass])
+            nextGlas = [0]*(len(glass)+1)
+            for j in range(len(glass)):
+                Q = (glass[j]-1)/2
+                if Q > 0:
+                    nextGlas[j] += Q
+                    nextGlas[j+1] += Q
+            glass = nextGlas
+        return min(1, glass[query_glass])
