6e6752284c72ec73a5f15491e9935f2c8b64b130
Satyam Tripathi <satyamtripathi5554@gmail.com>
2022-03-05 18:06:44 +0530

    Added solution - LeetHub

--- Target Sum - GFG/target-sum.py
+++ Target Sum - GFG/target-sum.py
@@ -1,7 +1,8 @@
 class Solution:
     def perfectSum(self, nums, n, total):
         dp = [[0]*(total+1) for _ in range(n+1)]
-        dp[0][0] = 1
+        for i in range(n+1):
+            dp[i][0] = 1
         for i in range(1, n+1):
             for j in range(total+1):
                 if nums[i-1] <= j:
@@ -9,13 +10,15 @@ class Solution:
                 else:
                     dp[i][j] = dp[i-1][j]
         return (dp[n][total])
-    def findTargetSumWays(self, nums, n, target):
-        hey  = (target + sum(nums))
-        if hey%2 != 0:
-           return 0
-        hey//=2
-        return self.perfectSum(nums, len(nums), hey)
 
+    def findTargetSumWays(self, nums, n, target):
+        nums_sum = sum(nums)
+        total = (target + nums_sum)
+        if total % 2 != 0 or abs(target) > nums_sum:
+            return 0
+        else:
+            total //= 2
+        return self.perfectSum(nums, len(nums), total)
 
 
 #{ 
