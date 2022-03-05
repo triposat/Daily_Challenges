8f6b8f98b9972a200999be3167f75b24f28dfcdc
Satyam Tripathi <satyamtripathi5554@gmail.com>
2022-03-05 11:12:42 +0530

    Time: 28 ms (95.73%), Space: 13.9 MB (56.33%) - LeetHub

--- 198-house-robber/198-house-robber.py
+++ 198-house-robber/198-house-robber.py
@@ -1,12 +1,13 @@
 class Solution:
     def rob(self, nums: List[int]) -> int:
-        if len(nums) == 1:
+        n = len(nums)
+        if n == 1:
             return nums[0]
-        if len(nums) == 2:
+        if n == 2:
             return max(nums[0], nums[1])
         two_back = 0
         one_back = 0
-        for i in range(len(nums)):
+        for i in range(n):
             two_back, one_back = one_back, max(
                 one_back, two_back+nums[i])
         return one_back
