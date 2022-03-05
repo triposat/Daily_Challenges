d209710feb17025bc701c606d31d76a13ab348e2
Satyam Tripathi <satyamtripathi5554@gmail.com>
2022-03-05 22:18:48 +0530

    Time: 92 ms (58.82%), Space: 14.3 MB (5.88%) - LeetHub

--- 2190-most-frequent-number-following-key-in-an-array/2190-most-frequent-number-following-key-in-an-array.py
+++ 2190-most-frequent-number-following-key-in-an-array/2190-most-frequent-number-following-key-in-an-array.py
@@ -1,11 +1,11 @@
+from collections import Counter
 class Solution:
     def mostFrequent(self, nums: List[int], key: int) -> int:
-        hey=Counter(nums)
-        cnt=0
+        hashMap = Counter(nums)
+        cnt = 0
         for i in range(len(nums)-1):
-            if nums[i]==key:
-                if hey[nums[i+1]]>cnt:
-                    cnt = hey[nums[i+1]]
-                    ans = nums[i+1]
-        return ans
-                
+            if nums[i] == key:
+                if hashMap[nums[i+1]] > cnt:
+                    cnt = hashMap[nums[i+1]]
+                    res = nums[i+1]
+        return res
