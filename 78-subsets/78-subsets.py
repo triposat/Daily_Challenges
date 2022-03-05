7c6b7123b607878d30cee7136ae6094b15beeebc
Satyam Tripathi <satyamtripathi5554@gmail.com>
2022-03-05 23:11:34 +0530

    Time: 36 ms (85.82%), Space: 14.2 MB (53.24%) - LeetHub

--- 78-subsets/78-subsets.py
+++ 78-subsets/78-subsets.py
@@ -1,15 +1,11 @@
 class Solution:
     def subsets(self, nums: List[int]) -> List[List[int]]:
-        res=[[]]
+        outer=[]
+        outer.append([])
         for num in nums:
-            res+=[curr+[num] for curr in res]
-        return res
-        # outer=[]
-        # outer.append([])
-        # for num in nums:
-        #     n=len(outer)
-        #     for i in range(n):
-        #         inner = outer[i][:]
-        #         inner.append(num)
-        #         outer.append(inner)
-        # return outer
+            n=len(outer)
+            for i in range(n):
+                inner = outer[i][:]
+                inner.append(num)
+                outer.append(inner)
+        return outer
