955547506dc282cf6c200b8cca367b46346357d3
Satyam Tripathi <satyamtripathi5554@gmail.com>
2022-03-06 12:08:05 +0530

    Time: 53 ms (48.01%), Space: 14 MB (71.02%) - LeetHub

---
+++ 1359-count-all-valid-pickup-and-delivery-options/1359-count-all-valid-pickup-and-delivery-options.py
@@ -0,0 +1,26 @@
+class Solution:
+    def countOrders(self, n: int) -> int:
+        return (math.factorial(2*n)>>n)%((10**9)+7)
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
