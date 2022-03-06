b068281abf6e62a7a083dc229c721d9551f3b8f5
Satyam Tripathi <satyamtripathi5554@gmail.com>
2022-03-06 14:33:23 +0530

    Time: 40 ms (74.43%), Space: 14.1 MB (56.82%) - LeetHub

--- 1359-count-all-valid-pickup-and-delivery-options/1359-count-all-valid-pickup-and-delivery-options.py
+++ 1359-count-all-valid-pickup-and-delivery-options/1359-count-all-valid-pickup-and-delivery-options.py
@@ -1,26 +1,9 @@
 class Solution:
     def countOrders(self, n: int) -> int:
-        return (math.factorial(2*n)>>n)%((10**9)+7)
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
+        # return (math.factorial(2*n)>>n)%((10**9)+7)
+        dp=[0 for i in range(n+1)]
+        dp[0] = 1
+        for i in range(1, n+1):
+            gaps = 2*i+1
+            dp[i] = dp[i-1]*(gaps*(gaps+1))//2
+        return dp[n-1]%((10**9)+7)
