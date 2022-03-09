a6db11d78ba059fcd0926511b77713f63f5ec1cc
Satyam Tripathi <satyamtripathi5554@gmail.com>
2022-03-09 23:21:41 +0530

    Time: 102 ms (15.42%), Space: 13.9 MB (68.55%) - LeetHub

--- 994-rotting-oranges/994-rotting-oranges.py
+++ 994-rotting-oranges/994-rotting-oranges.py
@@ -1,26 +1,25 @@
 class Solution:
-    def orangesRotting(self, mat: List[List[int]]) -> int:
+    def orangesRotting(self, grid: List[List[int]]) -> int:
         que = []
-        row, col = len(mat), len(mat[0])
-        fresh=0
+        row, col = len(grid), len(grid[0])
+        fresh = 0
         for i in range(row):
             for j in range(col):
-                if mat[i][j] == 1:
-                    fresh+=1
-                elif mat[i][j]==2:
+                if grid[i][j] == 1:
+                    fresh += 1
+                elif grid[i][j] == 2:
                     que.append((i, j))
         if not fresh:
             return 0
-        # print(que, fresh)
-        res=0
+        res = 0
         while que:
-            res+=1
+            res += 1
             for _ in range(len(que)):
                 i, j = que.pop(0)
                 for r, c in ((i, j+1), (i, j-1), (i+1, j), (i-1, j)):
-                    if 0 <= r < row and 0 <= c < col and mat[r][c] == 1:
-                        mat[r][c] = 2
-                        fresh-=1
+                    if 0 <= r < row and 0 <= c < col and grid[r][c] == 1:
+                        grid[r][c] = 2
+                        fresh -= 1
                         que.append((r, c))
-            
+
         return -1 if fresh else max(0, res-1)
