0a2bb004f470c79f0f2bf57398fa59cb98a89170
Satyam Tripathi <satyamtripathi5554@gmail.com>
2022-03-09 19:05:32 +0530

    Time: 929 ms (48.29%), Space: 17.1 MB (67.76%) - LeetHub

--- 542-01-matrix/542-01-matrix.py
+++ 542-01-matrix/542-01-matrix.py
@@ -1,17 +1,17 @@
 class Solution:
     def updateMatrix(self, mat: List[List[int]]) -> List[List[int]]:
-        q=[]
-        m, n = len(mat), len(mat[0])
-        for i in range(m):
-            for j in range(n):
-                if mat[i][j]==1:
-                    mat[i][j] = (1<<31)
+        que = []
+        row, col = len(mat), len(mat[0])
+        for i in range(row):
+            for j in range(col):
+                if mat[i][j] == 1:
+                    mat[i][j] = (1 << 31)
                 else:
-                    q.append((i, j))
-        for i, j in q:
+                    que.append((i, j))
+        for i, j in que:
             for r, c in ((i, j+1), (i, j-1), (i+1, j), (i-1, j)):
-                me=mat[i][j]+1
-                if 0<=r<m and 0<=c<n and mat[r][c]>me:
-                    mat[r][c] = me
-                    q.append((r, c))
+                k = mat[i][j]+1
+                if 0 <= r < row and 0 <= c < col and mat[r][c] > k:
+                    mat[r][c] = k
+                    que.append((r, c))
         return mat
