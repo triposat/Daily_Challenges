2d8ae118aa738df4362499db9c2fbeb048bdd790
Satyam Tripathi <satyamtripathi5554@gmail.com>
2022-03-09 11:46:16 +0530

    Time: 74 ms (21.61%), Space: 13.9 MB (85.61%) - LeetHub

--- 82-remove-duplicates-from-sorted-list-ii/82-remove-duplicates-from-sorted-list-ii.py
+++ 82-remove-duplicates-from-sorted-list-ii/82-remove-duplicates-from-sorted-list-ii.py
@@ -3,19 +3,20 @@
 #     def __init__(self, val=0, next=None):
 #         self.val = val
 #         self.next = next
+
 class Solution:
     def deleteDuplicates(self, head: Optional[ListNode]) -> Optional[ListNode]:
         if not head:
             return
-        temp=head
-        hey=ListNode(0)
-        me=hey
-        res=defaultdict(int)
+        temp = head
+        res = ListNode(0)
+        res1 = res
+        hasMap = defaultdict(int)
         while temp:
-            res[temp.val] = res.get(temp.val, 0)+1
-            temp=temp.next
-        for ele in res:
-            if res[ele]==1:
-                hey.next=ListNode(ele)
-                hey=hey.next
-        return me.next
+            hasMap[temp.val] = hasMap.get(temp.val, 0)+1
+            temp = temp.next
+        for ele in hasMap:
+            if hasMap[ele] == 1:
+                res.next = ListNode(ele)
+                res = res.next
+        return res1.next
