4f9fa11f6f306b9dd663748e4c73756366a26d92
Satyam Tripathi <satyamtripathi5554@gmail.com>
2022-03-11 16:24:22 +0530

    Time: 582 ms (44.43%), Space: 15.9 MB (65.55%) - LeetHub

---
+++ 77-combinations/77-combinations.py
@@ -0,0 +1,12 @@
+class Solution:
+    def combine(self, n: int, k: int) -> List[List[int]]:
+        res = []
+        def solve(nums, k, path):
+            if len(path) == k:
+                res.append(path)
+                return
+            for i in range(len(nums)):
+                solve(nums[i+1:], k, path+[nums[i]])
+
+        solve(list(range(1, n+1)), k, [])
+        return res
