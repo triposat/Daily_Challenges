9c81ae8ccbbce140537397d4c0be388047498b3f
Satyam Tripathi <satyamtripathi5554@gmail.com>
2022-03-13 20:03:24 +0530

    Time: 858 ms (71.24%), Space: 32.3 MB (5.16%) - LeetHub

---
+++ 2195-append-k-integers-with-minimal-sum/2195-append-k-integers-with-minimal-sum.py
@@ -0,0 +1,19 @@
+class Solution:
+    def minimalKSum(self, nums: List[int], k: int) -> int:
+        hMap = Counter(nums)
+        start = 1
+        ans = 0
+        for key in sorted(hMap):
+            gap = key-start
+            s = start
+            e = key
+            if k >= gap:
+                k -= gap
+            else:
+                break
+            ans += e*(e-1)//2 - s*(s-1)//2
+            start = e+1
+        s = start
+        e = start+k
+        ans += e*(e-1)//2 - s*(s-1)//2
+        return ans
