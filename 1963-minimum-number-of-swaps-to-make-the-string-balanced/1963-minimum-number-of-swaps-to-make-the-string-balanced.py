eead357d62f7ca7b729e333214631476cf4846a9
Satyam Tripathi <satyamtripathi5554@gmail.com>
2022-03-15 12:53:13 +0530

    Time: 441 ms (77.31%), Space: 26.9 MB (23.86%) - LeetHub

--- 1963-minimum-number-of-swaps-to-make-the-string-balanced/1963-minimum-number-of-swaps-to-make-the-string-balanced.py
+++ 1963-minimum-number-of-swaps-to-make-the-string-balanced/1963-minimum-number-of-swaps-to-make-the-string-balanced.py
@@ -1,13 +1,23 @@
 class Solution:
     def minSwaps(self, s: str) -> int:
-        mism=0
-        stack=[]
+        # Space: O(n)
+        mism = 0
+        stack = []
         for i in s:
-            if i=='[':
+            if i == '[':
                 stack.append(i)
             else:
                 if stack:
                     stack.pop()
                 else:
-                    mism+=1
+                    mism += 1
         return (mism+1)//2
+        # Space: O(1)
+        stack = 0
+        for i in s:
+            if i == '[':
+                stack += 1
+            else:
+                if stack > 0:
+                    stack -= 1
+        return (stack+1)//2
