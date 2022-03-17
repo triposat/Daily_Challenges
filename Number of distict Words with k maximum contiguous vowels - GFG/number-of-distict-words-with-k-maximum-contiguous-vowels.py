24853195253c67d0b6035972531a298880641c3a
Satyam Tripathi <satyamtripathi5554@gmail.com>
2022-03-17 23:19:43 +0530

    Added solution - LeetHub

--- Number of distict Words with k maximum contiguous vowels - GFG/number-of-distict-words-with-k-maximum-contiguous-vowels.py
+++ Number of distict Words with k maximum contiguous vowels - GFG/number-of-distict-words-with-k-maximum-contiguous-vowels.py
@@ -1,14 +1,13 @@
-# Memoization
 class Solution:
-    def kvowelwords(self, n,k):
-        mod=1000000007
-        dp=[[1]*(k+1) for _ in range(n+1)]
+    def kvowelwords(self, n, k):
+        mod = 1000000007
+        dp = [[1]*(k+1) for _ in range(n+1)]
         for i in range(1, n+1):
             for j in range(0, k+1):
-                if j==0:
-                    dp[i][j] = 21*dp[i-1][k]%mod
+                if j == 0:
+                    dp[i][j] = (21*dp[i-1][k]) % mod
                 else:
-                    dp[i][j] = (21*dp[i-1][k]%mod+5*dp[i-1][j-1]%mod)%mod
+                    dp[i][j] = (21*dp[i-1][k] % mod+5*dp[i-1][j-1] % mod) % mod
         return dp[n][k]
 
 #{ 
