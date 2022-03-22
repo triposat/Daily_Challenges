98309f6604d42888e1be05ffac5328fa3eef4d2f
Satyam Tripathi <satyamtripathi5554@gmail.com>
2022-03-22 21:28:45 +0530

    Time: 953 ms (42.86%), Space: 15.3 MB (71.43%) - LeetHub

---
+++ 1663-smallest-string-with-a-given-numeric-value/1663-smallest-string-with-a-given-numeric-value.py
@@ -0,0 +1,9 @@
+class Solution:
+    def getSmallestString(self, n: int, k: int) -> str:
+        res = ['a']*n
+        k-=n
+        while k:
+            res[n-1] = chr(97+min(25, k))
+            k-=min(25, k)
+            n-=1
+        return "".join(res)
