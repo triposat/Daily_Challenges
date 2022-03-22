877ac550db340682deb88dbb4d00cc4c68352e64
Satyam Tripathi <satyamtripathi5554@gmail.com>
2022-03-23 01:33:15 +0530

    Time: 1270 ms (6.86%), Space: 22.5 MB (5.17%) - LeetHub

--- 2211-count-collisions-on-a-road/2211-count-collisions-on-a-road.py
+++ 2211-count-collisions-on-a-road/2211-count-collisions-on-a-road.py
@@ -1,13 +1,14 @@
 class Solution:
-    def countCollisions(self, d: str) -> int:
+    def countCollisions(self, directions: str) -> int:
+        # 1st Approach
         colli = []
         i = 0
-        while i < len(d):
+        while i < len(directions):
             cnt = 1
-            while i+1 < len(d) and d[i] == d[i+1]:
+            while i+1 < len(directions) and directions[i] == directions[i+1]:
                 cnt += 1
                 i += 1
-            colli.append((cnt, d[i]))
+            colli.append((cnt, directions[i]))
             i += 1
         ans = 0
         for i in range(len(colli)-1):
@@ -17,4 +18,20 @@ class Solution:
                 ans += colli[i][0]
             elif colli[i][1] == "S" and colli[i+1][1] == "L":
                 ans += colli[i+1][0]
+        return ans
+        
+        # 2nd Approach
+        ans = 0
+        temp = 0
+        for dire in directions:
+            if dire == "L":
+                ans += temp
+            else:
+                temp = 1
+        temp = 0
+        for dire in directions[::-1]:
+            if dire == "R":
+                ans += temp
+            else:
+                temp = 1
         return ans
