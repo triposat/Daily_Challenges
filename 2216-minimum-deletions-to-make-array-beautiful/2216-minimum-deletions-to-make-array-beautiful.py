6f8501105ea0f98204c87977f33a170c3662354d
Satyam Tripathi <satyamtripathi5554@gmail.com>
2022-03-27 12:17:29 +0530

    Time: 1873 ms (33.33%), Space: 28.5 MB (33.33%) - LeetHub

--- 2216-minimum-deletions-to-make-array-beautiful/2216-minimum-deletions-to-make-array-beautiful.py
+++ 2216-minimum-deletions-to-make-array-beautiful/2216-minimum-deletions-to-make-array-beautiful.py
@@ -1,17 +1,11 @@
 class Solution:
     def minDeletion(self, nums: List[int]) -> int:
         cnt = 0
-        chnge = 'e'
         n = len(nums)
         for i in range(len(nums)-1):
-            if chnge == 'e':
-                if i % 2 == 0 and nums[i] == nums[i+1]:
+            if (i+cnt) % 2 == 0:
+                if nums[i] == nums[i+1]:
                     cnt += 1
-                    chnge = 'o'
-            elif chnge == 'o':
-                if i % 2 != 0 and nums[i] == nums[i+1]:
-                    cnt += 1
-                    chnge = 'e'
         if (n-cnt) % 2 != 0:
             cnt += 1
         return cnt
