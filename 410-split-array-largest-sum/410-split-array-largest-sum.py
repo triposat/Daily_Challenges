5e4dd8b046d738cb1f9360ebf6c61f3776b2effe
Satyam Tripathi <satyamtripathi5554@gmail.com>
2022-04-08 18:08:05 +0530

    Time: 32 ms (98.28%), Space: 14 MB (38.87%) - LeetHub

--- 410-split-array-largest-sum/410-split-array-largest-sum.py
+++ 410-split-array-largest-sum/410-split-array-largest-sum.py
@@ -1,31 +1,25 @@
-# Time: O(n * logK), where K is the size of the search space (sum of weights - max weight).
-# Space: O(1)
 class Solution:
-    def isPossible(self, nums, mid, m):
+    def possible_subA(self, nums, mid):
+        add = 0
         cnt = 1
-        sumAllocated = 0
         for i in range(len(nums)):
-            if(sumAllocated > mid):
-                return False
-            sumAllocated += nums[i]
-            if(sumAllocated > mid):
+            if add+nums[i] > mid:
+                add = nums[i]
                 cnt += 1
-                sumAllocated = nums[i]
-        if (cnt <= m):
-            return True
-        return False
+            else:
+                add += nums[i]
+        return cnt
 
     def splitArray(self, nums: List[int], m: int) -> int:
-        if m > len(nums):
-            return -1
         low = max(nums)
         high = sum(nums)
         ans = 0
         while low <= high:
             mid = (low+high) >> 1
-            if self.isPossible(nums, mid, m):
-                ans = mid
-                high = mid-1
-            else:
+            n = self.possible_subA(nums, mid)
+            if n > m:
                 low = mid+1
-        return ans  # Or we can return low!
+            else:
+                high = mid-1
+                ans = mid
+        return ans
