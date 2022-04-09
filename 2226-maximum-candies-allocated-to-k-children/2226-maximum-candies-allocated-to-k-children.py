dd6f10d0c74e57c82bbb4cfc34c61554792bf977
Satyam Tripathi <satyamtripathi5554@gmail.com>
2022-04-09 07:39:30 +0530

    Time: 2084 ms (42.54%), Space: 27.3 MB (74.33%) - LeetHub

--- 2226-maximum-candies-allocated-to-k-children/2226-maximum-candies-allocated-to-k-children.py
+++ 2226-maximum-candies-allocated-to-k-children/2226-maximum-candies-allocated-to-k-children.py
@@ -1,14 +1,14 @@
 class Solution:
     def maximumCandies(self, cand: List[int], k: int) -> int:
-        s=0
-        e=sum(cand)
-        while s<e:
-            ans=0
-            mid = (s+e+1)>>1
+        left = 0
+        right = sum(cand)
+        while left < right:
+            ans = 0
+            mid = (left+right+1) >> 1
             for i in cand:
-                ans+=i//mid
-            if ans>=k:
-                s=mid
+                ans += i//mid
+            if ans >= k:
+                left = mid
             else:
-                e=mid-1
-        return s
+                right = mid-1
+        return left
