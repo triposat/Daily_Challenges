b94292d2bb40f8f1981484a7ed8fef2664a132f1
Satyam Tripathi <satyamtripathi5554@gmail.com>
2022-04-12 05:45:25 +0530

    Time: 164 ms (95.22%), Space: 14.3 MB (60.95%) - LeetHub

--- 1260-shift-2d-grid/1260-shift-2d-grid.py
+++ 1260-shift-2d-grid/1260-shift-2d-grid.py
@@ -1,21 +1,9 @@
-# class Solution:
-#     def shiftGrid(self, grid: List[List[int]], k: int) -> List[List[int]]:
-#         m=len(grid)
-#         n=len(grid[0])
-#         hey = grid[-1][-1]
-#         me = grid[0].pop()
-#         for i in range(k):
-#             for j in range(1, m):
-#                 grid[j].insert(0, me)
-#                 me = grid[j].pop()
-#             grid[0].insert(0, me)
-#         return grid
-
 class Solution:
     def shiftGrid(self, grid: List[List[int]], k: int) -> List[List[int]]:
-            k = k % (len(grid) * len(grid[0]))
-            print(k)
-            for _ in range(k):
-                for i in range(len(grid)):
-                    grid[i] = [grid[i-1].pop()] + grid[i]
-            return grid
+        m = len(grid)
+        n = len(grid[0])
+        k = k % (m*n)
+        for _ in range(k):
+            for j in range(m):
+                grid[j].insert(0, grid[j-1].pop())
+        return grid
