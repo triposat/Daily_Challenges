04ad4da34bcc222941e44246cd33c244d1832df4
Satyam Tripathi <satyamtripathi5554@gmail.com>
2022-04-15 00:40:41 +0530

    Time: 35 ms (83.02%), Space: 14 MB (8.98%) - LeetHub

--- 59-spiral-matrix-ii/59-spiral-matrix-ii.py
+++ 59-spiral-matrix-ii/59-spiral-matrix-ii.py
@@ -5,24 +5,24 @@ class Solution:
         col_bg = 0
         row_end = len(mat)
         col_end = len(mat[0])
-        val=1
+        val = 1
         while row_bg < row_end and col_bg < col_end:
             for i in range(col_bg, col_end):
                 mat[row_bg][i] = val
-                val+=1
+                val += 1
             row_bg += 1
             for i in range(row_bg, row_end):
-                mat[i][col_end-1]=val
-                val+=1
+                mat[i][col_end-1] = val
+                val += 1
             col_end -= 1
             if row_bg < row_end:
                 for i in range(col_end-1, col_bg-1, -1):
-                    mat[row_end-1][i]= val
-                    val+=1
+                    mat[row_end-1][i] = val
+                    val += 1
             row_end -= 1
             if col_bg < col_end:
                 for i in range(row_end-1, row_bg-1, -1):
                     mat[i][col_bg] = val
-                    val+=1
+                    val += 1
             col_bg += 1
         return mat
