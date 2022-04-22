7d3a386044ca9752bd305dc99e466fb0e7d27c05
Satyam Tripathi <satyamtripathi5554@gmail.com>
2022-04-23 03:04:57 +0530

    Time: 426 ms (40.66%), Space: 39.7 MB (15.90%) - LeetHub

---
+++ 706-design-hashmap/706-design-hashmap.py
@@ -0,0 +1,34 @@
+class MyHashMap(object):
+
+    def __init__(self):
+        """
+        Initialize your data structure here.
+        """
+        self.table = [-1] * 1000001
+
+    def put(self, key, value):
+        """
+        value will always be non-negative.
+        :type key: int
+        :type value: int
+        :rtype: void
+        """
+        self.table[key] = value
+        
+
+    def get(self, key):
+        """
+        Returns the value to which the specified key is mapped, or -1 if this map contains no mapping for the key
+        :type key: int
+        :rtype: int
+        """
+        return self.table[key]
+        
+
+    def remove(self, key):
+        """
+        Removes the mapping of the specified value key if this map contains a mapping for the key
+        :type key: int
+        :rtype: void
+        """
+        self.table[key] = -1
