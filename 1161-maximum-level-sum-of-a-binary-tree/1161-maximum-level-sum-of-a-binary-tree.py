e0ef596b33938f7daaad5117d9e9507b121b4d17
Satyam Tripathi <satyamtripathi5554@gmail.com>
2022-04-26 19:03:52 +0530

    Time: 334 ms (74.70%), Space: 18.9 MB (7.86%) - LeetHub

--- 1161-maximum-level-sum-of-a-binary-tree/1161-maximum-level-sum-of-a-binary-tree.py
+++ 1161-maximum-level-sum-of-a-binary-tree/1161-maximum-level-sum-of-a-binary-tree.py
@@ -4,21 +4,37 @@
 #         self.val = val
 #         self.left = left
 #         self.right = right
+
+
+# Iterative - BFS Solution
+# class Solution:
+#     def maxLevelSum(self, root: Optional[TreeNode]) -> int:
+#         if not root:
+#             return None
+#         que = [root]
+#         res = []
+#         while que:
+#             temp = []
+#             for _ in range(len(que)):
+#                 ele = que.pop(0)
+#                 temp.append(ele.val)
+#                 if ele.left:
+#                     que.append(ele.left)
+#                 if ele.right:
+#                     que.append(ele.right)
+#             res.append(sum(temp))
+#         res = enumerate(res)
+#         return max(res, key=lambda x: x[1])[0]+1
+    
+# Recursive Solution
 class Solution:
     def maxLevelSum(self, root: Optional[TreeNode]) -> int:
-        if not root:
-            return None
-        que = [root]
-        res = []
-        while que:
-            temp = []
-            for _ in range(len(que)):
-                ele = que.pop(0)
-                temp.append(ele.val)
-                if ele.left:
-                    que.append(ele.left)
-                if ele.right:
-                    que.append(ele.right)
-            res.append(sum(temp))
-        res = enumerate(res)
-        return max(res, key=lambda x: x[1])[0]+1
+        levels = defaultdict(int)
+
+        def ans(root, d):
+            if root:
+                levels[d] += root.val
+                ans(root.left, d+1)
+                ans(root.right, d+1)
+        ans(root, 1)
+        return max(levels, key=levels.get)
