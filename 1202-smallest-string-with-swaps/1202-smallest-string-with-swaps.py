48282e262de16df6f4d3bf32800c3762d18d4df2
Satyam Tripathi <satyamtripathi5554@gmail.com>
2022-04-29 01:29:08 +0530

    Time: 743 ms (83.28%), Space: 71.9 MB (21.09%) - LeetHub

--- 1202-smallest-string-with-swaps/1202-smallest-string-with-swaps.py
+++ 1202-smallest-string-with-swaps/1202-smallest-string-with-swaps.py
@@ -1,12 +1,12 @@
 class Solution:
     def smallestStringWithSwaps(self, s: str, pairs: List[List[int]]) -> str:
         def dfs(i):
-            visit[i]=True
-            ans.append(i)
+            visit[i] = True
+            res.append(i)
             for j in adj[i]:
                 if not visit[j]:
                     dfs(j)
-        n=len(s)
+        n = len(s)
         adj = [[] for _ in range(n)]
         for a, b in pairs:
             adj[a].append(b)
@@ -15,14 +15,11 @@ class Solution:
         lst = list(s)
         for i in range(n):
             if not visit[i]:
-                ans=[]
+                res = []
                 dfs(i)
-                # print(ans)
-                ans.sort()
-                chars = [lst[k] for k in ans]
-                print(chars)
-                chars.sort()
-                print(chars)
-                for i in range(len(ans)):
-                    lst[ans[i]] = chars[i]
-        return ''.join(lst)
+                res.sort()
+                char = [lst[j] for j in res]
+                char.sort()
+                for k in range(len(res)):
+                    lst[res[k]] = char[k]
+        return "".join(lst)
