4b424da409c8d85a5b92b523b0974093f44984aa
Satyam Tripathi <satyamtripathi5554@gmail.com>
2022-05-03 04:19:30 +0530

    Time: 140 ms (19.97%), Space: 14.7 MB (63.64%) - LeetHub

---
+++ 905-sort-array-by-parity/905-sort-array-by-parity.py
@@ -0,0 +1,11 @@
+class Solution:
+    def sortArrayByParity(self, A):
+        beg, end = 0, len(A) - 1
+        
+        while beg <= end:
+            if A[beg] % 2 == 0:
+                beg += 1
+            else:
+                A[beg], A[end] = A[end], A[beg]
+                end -= 1
+        return A
