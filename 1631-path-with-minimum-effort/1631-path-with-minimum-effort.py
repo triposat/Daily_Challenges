cb462fb7dac97ee0383c736a0ff53589bee6ec7f
Satyam Tripathi <satyamtripathi5554@gmail.com>
2022-05-02 23:08:49 +0530

    Time: 925 ms (65.40%), Space: 15.9 MB (55.74%) - LeetHub

---
+++ 1631-path-with-minimum-effort/1631-path-with-minimum-effort.py
@@ -0,0 +1,26 @@
+from heapq import heappop, heappush, heapify
+
+class Solution:
+    def minimumEffortPath(self, h: List[List[int]]) -> int:
+        direc = [[0, 1], [1, 0], [0, -1], [-1, 0]]
+        row = len(h)
+        col = len(h[0])
+        pq = []
+        heapify(pq)
+        ans = 0
+        heappush(pq, (0, 0, 0))
+        while pq:
+            ele = heappop(pq)
+            ans = max(ans, ele[0])
+            i = ele[1]
+            j = ele[2]
+            if i == row-1 and j == col-1:
+                break
+            for di in direc:
+                newi = i+di[0]
+                newj = j+di[1]
+                if newi < 0 or newi >= row or newj < 0 or newj >= col or h[i][j] == -1:
+                    continue
+                heappush(pq, (abs(h[newi][newj]-h[i][j]), newi, newj))
+            h[i][j] = -1
+        return ans
