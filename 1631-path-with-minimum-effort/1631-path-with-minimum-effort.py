23639b80dbc5f16d0fec4c5b5c75576c20610bd9
Satyam Tripathi <satyamtripathi5554@gmail.com>
2022-05-02 23:10:10 +0530

    Time: 1881 ms (33.62%), Space: 16.1 MB (47.04%) - LeetHub

--- 1631-path-with-minimum-effort/1631-path-with-minimum-effort.py
+++ 1631-path-with-minimum-effort/1631-path-with-minimum-effort.py
@@ -1,5 +1,6 @@
 from heapq import heappop, heappush, heapify
 
+
 class Solution:
     def minimumEffortPath(self, h: List[List[int]]) -> int:
         direc = [[0, 1], [1, 0], [0, -1], [-1, 0]]
@@ -14,14 +15,14 @@ class Solution:
             ans = max(ans, ele[0])
             i = ele[1]
             j = ele[2]
-            if h[i][j]==-1:
+            if h[i][j] == -1:
                 continue
             if i == row-1 and j == col-1:
                 break
             for di in direc:
                 newi = i+di[0]
                 newj = j+di[1]
-                if newi < 0 or newi >= row or newj < 0 or newj >= col or h[i][j] == -1:
+                if newi < 0 or newi >= row or newj < 0 or newj >= col:
                     continue
                 heappush(pq, (abs(h[newi][newj]-h[i][j]), newi, newj))
             h[i][j] = -1
