29f985f606626b93b49a0c1e7f1d922e153ebae0
Satyam Tripathi <satyamtripathi5554@gmail.com>
2022-05-03 05:35:21 +0530

    Time: 31 ms (90.16%), Space: 13.9 MB (76.22%) - LeetHub

--- 844-backspace-string-compare/844-backspace-string-compare.py
+++ 844-backspace-string-compare/844-backspace-string-compare.py
@@ -1,15 +1,47 @@
+# Time: O(m+n)
+# Space: O(m+n)
+# class Solution:
+#     def backspaceCompare(self, s: str, t: str) -> bool:
+#         def stack(string):
+#             res = []
+#             for var in string:
+#                 if var != "#":
+#                     res.append(var)
+#                 else:
+#                     if not res:
+#                         continue
+#                     res.pop()
+#             return res
+#         s = stack(s)
+#         t = stack(t)
+#         return s == t
+# Time: (m+n)
+# Space: O(1)
 class Solution:
     def backspaceCompare(self, s: str, t: str) -> bool:
-        def stack(string):
-            res=[]
-            for var in string:
-                if var!="#":
-                    res.append(var)
-                else:
-                    if not res:
-                        continue
-                    res.pop()
-            return res
-        s=stack(s)
-        t=stack(t)
-        return s==t
+        s=list(s)
+        t=list(t)
+        k = 0
+        for i in range(len(s)):
+            if s[i] != '#':
+                s[k] = s[i]
+                k += 1
+            else:
+                k -= 1
+                k = max(0, k)
+        l = 0
+        for i in range(len(t)):
+            if t[i] != '#':
+                t[l] = t[i]
+                l += 1
+            else:
+                l -= 1
+                l = max(0, l)
+        if k != l:
+            return False
+        else:
+            for i in range(k):
+                if s[i] != t[i]:
+                    return False
+            return True
+                
