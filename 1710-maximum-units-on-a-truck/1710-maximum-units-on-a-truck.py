a17a1b344cc7cd736e96d4d50ce30a05bc1a65ca
Satyam Tripathi <satyamtripathi5554@gmail.com>
2022-07-02 01:27:40 +0530

    Time: 277 ms (33.37%), Space: 14.5 MB (32.66%) - LeetHub

--- 1710-maximum-units-on-a-truck/1710-maximum-units-on-a-truck.py
+++ 1710-maximum-units-on-a-truck/1710-maximum-units-on-a-truck.py
@@ -1,5 +1,8 @@
+############################## My First Approach...
+
 # Time: O(nlogn)
 # Space: O(n)
+
 # class Solution:
 #     def maximumUnits(self, boxTypes: List[List[int]], truckSize: int) -> int:
 #         boxTypes.sort(key=lambda x: x[1], reverse=True)
@@ -18,6 +21,11 @@
 #                     return res
 #         return res
 
+######################## More Precisely written approach
+
+# Time: O(nlogn)
+# Space: O(n)
+
 # class Solution:
 #     def maximumUnits(self, boxTypes: List[List[int]], truckSize: int) -> int:
 #         boxTypes.sort(key=lambda x: -x[1])
@@ -31,14 +39,39 @@
 #                 return res
 #         return res
 
+########################## More Precise Approach 
 
+# # Time: O(nlogn)
+# # Space: O(n)
+    
+# class Solution:
+#     def maximumUnits(self, boxTypes: List[List[int]], truckSize: int) -> int:
+#         boxTypes.sort(key=lambda x: -x[1])
+#         res = 0
+#         for box, units in boxTypes:
+#             res += min(truckSize, box)*units
+#             truckSize -= box
+#             if truckSize <= 0:
+#                 return res
+#         return res
+
+######################### Best Optimal Approach
+# Time: O(n)
+# Space: O(n)
+# Counting Sort Techniques...
 class Solution:
     def maximumUnits(self, boxTypes: List[List[int]], truckSize: int) -> int:
-        boxTypes.sort(key=lambda x: -x[1])
-        res = 0
+        cntr = Counter()
         for box, units in boxTypes:
-            res += min(truckSize, box)*units
-            truckSize -= box
+            cntr[units] += box
+        temp = 1000
+        res = 0
+        while temp > 0:
+            ans = min(truckSize, cntr[temp])
+            res += (ans*temp)
+            truckSize -= ans
             if truckSize <= 0:
                 return res
+            temp -= 1
         return res
+            
