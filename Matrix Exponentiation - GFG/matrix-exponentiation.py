5154a229bd321300d679b364c5df1a0c508336ab
Satyam Tripathi <satyamtripathi5554@gmail.com>
2022-07-02 23:10:50 +0530

    Added solution - LeetHub

--- Matrix Exponentiation - GFG/matrix-exponentiation.py
+++ Matrix Exponentiation - GFG/matrix-exponentiation.py
@@ -1,23 +1,24 @@
 class Solution:
     def FindNthTerm(self, n):
         m = (10**9)+7
-        def multi(m1, m2):
+        def multi(mat, ans):
             res = [[0, 0], [0, 0]]
-            for i in range(len(m1)):
-                for j in range(len(m2)):
-                    for k in range(len(m1)):
-                        res[i][j] += (m1[i][k]%m)*(m2[k][j]%m)
+            for i in range(len(mat)):
+                for j in range(len(ans)):
+                    for k in range(len(mat)):
+                        res[i][j] += (mat[i][k]%m)*(ans[k][j]%m)
             return res
-        m1 = [[1, 1], [1, 0]]
-        m2 = [[1, 1], [1, 0]]
+
+        mat = [[1, 1], [1, 0]]
+        ans = [[1, 1], [1, 0]]
         while n:
             if n & 1:
-                m2 = multi(m1, m2)
+                ans = multi(mat, ans)
                 n -= 1
             else:
-                m1 = multi(m1, m1)
+                mat = multi(mat, mat)
                 n >>= 1
-        return m2[0][1]%m
+        return ans[0][1]%m
 	    
 	    
 #{ 
