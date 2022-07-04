fa5075c8bdcce73b925d6a28b29746c950d71a7e
Satyam Tripathi <satyamtripathi5554@gmail.com>
2022-07-05 00:02:31 +0530

    Time: 61 ms (48.00%), Space: 14.1 MB (45.05%) - LeetHub

--- 139-word-break/139-word-break.py
+++ 139-word-break/139-word-break.py
@@ -1,30 +1,30 @@
-# # Recursion
-# # Time: (2^n)
-# class Solution:
-#     def wordBreak(self, s: str, wordDict: List[str]) -> bool:
-#         def helper(s):
-#             if not s:
-#                 return True
-#             for word in wordDict:
-#                 if word == s[0:len(word)]:
-#                     if helper(s[len(word):]):
-#                         return True
-#             return False
-#         return helper(s)
+# Recursion
+# Time: (2^n)
+class Solution:
+    def wordBreak(self, s: str, wordDict: List[str]) -> bool:
+        def helper(s):
+            if not s:
+                return True
+            for word in wordDict:
+                if word == s[0:len(word)]:
+                    if helper(s[len(word):]):
+                        return True
+            return False
+        return helper(s)
     
-# # Recursion: Very Optimal then the above code
-# class Solution:
-#     def wordBreak(self, s: str, wordDict: List[str]) -> bool:
-#         @cache
-#         def helper(s):
-#             if not s:
-#                 return True
-#             for word in wordDict:
-#                 if word == s[0:len(word)]:
-#                     if helper(s[len(word):]):
-#                         return True
-#             return False
-#         return helper(s)
+# Recursion: Very Optimal then the above code
+class Solution:
+    def wordBreak(self, s: str, wordDict: List[str]) -> bool:
+        @cache
+        def helper(s):
+            if not s:
+                return True
+            for word in wordDict:
+                if word == s[0:len(word)]:
+                    if helper(s[len(word):]):
+                        return True
+            return False
+        return helper(s)
     
 # DP
 class Solution:
