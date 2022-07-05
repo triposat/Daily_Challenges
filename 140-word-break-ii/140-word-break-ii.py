deef04ea99461adf854188becb1134cfc46df9d6
Satyam Tripathi <satyamtripathi5554@gmail.com>
2022-07-05 19:06:52 +0530

    Time: 59 ms (16.57%), Space: 13.9 MB (33.42%) - LeetHub

--- 140-word-break-ii/140-word-break-ii.py
+++ 140-word-break-ii/140-word-break-ii.py
@@ -1,9 +1,33 @@
+# DP
+# Time Complexity: O(n^3)
+# class Solution:
+#     def helper(self, s, words, dp):
+#         if s in dp:
+#             return dp[s]
+#         if not s:
+#             return []
+#         res = []
+#         for word in words:
+#             if not s.startswith(word):
+#                 continue
+#             if len(word) == len(s):
+#                 res.append(word)
+#             else:
+#                 rest = self.helper(s[len(word):], words, dp)
+#                 for item in rest:
+#                     ans = word + ' ' + item
+#                     res.append(ans)
+                    
+#         dp[s] = res
+#         return res
+
+#     def wordBreak(self, s: str, words: List[str]) -> List[str]:
+#         return self.helper(s, words, {})
+
+
+
 class Solution:
     def helper(self, s, words, dp):
-        if s in dp:
-            return dp[s]
-        if not s:
-            return []
         res = []
         for word in words:
             if not s.startswith(word):
@@ -15,7 +39,6 @@ class Solution:
                 for item in rest:
                     ans = word + ' ' + item
                     res.append(ans)
-        dp[s] = res
         return res
 
     def wordBreak(self, s: str, words: List[str]) -> List[str]:
