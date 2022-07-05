1159cb1f0d2d6b7d8bbee54a9d216bf059c92141
Satyam Tripathi <satyamtripathi5554@gmail.com>
2022-07-05 19:15:21 +0530

    Time: 38 ms (74.30%), Space: 13.9 MB (33.42%) - LeetHub

--- 140-word-break-ii/140-word-break-ii.py
+++ 140-word-break-ii/140-word-break-ii.py
@@ -1,31 +1,32 @@
 # DP
 # Time Complexity: O(n^3)
-# class Solution:
-#     def helper(self, s, words, dp):
-#         if s in dp:
-#             return dp[s]
-#         if not s:
-#             return []
-#         res = []
-#         for word in words:
-#             if not s.startswith(word):
-#                 continue
-#             if len(word) == len(s):
-#                 res.append(word)
-#             else:
-#                 rest = self.helper(s[len(word):], words, dp)
-#                 for item in rest:
-#                     ans = word + ' ' + item
-#                     res.append(ans)
+class Solution:
+    def helper(self, s, words, dp):
+        if s in dp:
+            return dp[s]
+        if not s:
+            return []
+        res = []
+        for word in words:
+            if not s.startswith(word):
+                continue
+            if len(word) == len(s):
+                res.append(word)
+            else:
+                rest = self.helper(s[len(word):], words, dp)
+                for item in rest:
+                    ans = word + ' ' + item
+                    res.append(ans)
                     
-#         dp[s] = res
-#         return res
+        dp[s] = res
+        return res
 
-#     def wordBreak(self, s: str, words: List[str]) -> List[str]:
-#         return self.helper(s, words, {})
+    def wordBreak(self, s: str, words: List[str]) -> List[str]:
+        return self.helper(s, words, {})
 
 
-
+# Recursion
+# Time: 
 class Solution:
     def helper(self, s, words, dp):
         res = []
