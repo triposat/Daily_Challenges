e80bbdc1c70db6e8978dfcee7fca59e4da6e475a
Satyam Tripathi <satyamtripathi5554@gmail.com>
2022-07-06 18:39:42 +0530

    Time: 213 ms (38.67%), Space: 14.9 MB (15.72%) - LeetHub

--- 73-set-matrix-zeroes/73-set-matrix-zeroes.py
+++ 73-set-matrix-zeroes/73-set-matrix-zeroes.py
@@ -1,24 +1,24 @@
 # Space: O(m+n)
-# class Solution:
-#     def setZeroes(self, matrix: List[List[int]]) -> None:
-#         """
-#         Do not return anything, modify matrix in-place instead.
-#         """
-#         if not matrix:
-#             return []
-#         m = len(matrix)
-#         n = len(matrix[0])
-#         rowZero = [False]*m # m Space
-#         colZero = [False]*n # n Space
-#         for row in range(m):
-#             for col in range(n):
-#                 if not matrix[row][col]:
-#                     rowZero[row] = True
-#                     colZero[col] = True
-#         for row in range(m):
-#             for col in range(n):
-#                 if rowZero[row] or colZero[col]:
-#                     matrix[row][col] = 0
+class Solution:
+    def setZeroes(self, matrix: List[List[int]]) -> None:
+        """
+        Do not return anything, modify matrix in-place instead.
+        """
+        if not matrix:
+            return []
+        m = len(matrix)
+        n = len(matrix[0])
+        rowZero = [False]*m # m Space
+        colZero = [False]*n # n Space
+        for row in range(m):
+            for col in range(n):
+                if not matrix[row][col]:
+                    rowZero[row] = True
+                    colZero[col] = True
+        for row in range(m):
+            for col in range(n):
+                if rowZero[row] or colZero[col]:
+                    matrix[row][col] = 0
                     
 # Space: O(1)
 class Solution:
